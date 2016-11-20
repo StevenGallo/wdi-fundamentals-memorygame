6cc96a272bf73076932d4c5044c9457737457c5a
Steven <mac@macs-MBP.home>
2016-11-20 01:58:22 -0500

    added first game logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -3,3 +3,21 @@ var CardOne = "queen";
 var CardTwo = "queen";
 var CardThree = "king";
 var CardFour = "king";
+if (CardOne === CardTwo){
+	alert("You found a match")
+}else alert("Sorry try again.")
+if (CardOne === CardThree){
+	alert("You found a match")
+}else alert("Sorry try again.")
+if (CardOne === CardFour){
+	alert("You found a match")
+}else alert("Sorry try again.")
+if (CardTwo === CardThree){
+	alert("You found a match")
+}else alert("Sorry try again.")
+if (CardTwo === CardFour){
+	alert("You found a match")
+}else alert("Sorry try again.")
+if (CardThree === CardFour){
+	alert("You found a match")
+}else alert("Sorry try again.")
