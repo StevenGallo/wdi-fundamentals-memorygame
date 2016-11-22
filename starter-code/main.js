e47668bd455a61375d7efb89fb04012eacb55a18
Steven <mac@macs-MBP.home>
2016-11-21 20:43:05 -0500

    added cards to arrays and added images

--- starter-code/main.js
+++ starter-code/main.js
@@ -22,13 +22,43 @@ if (CardTwo === CardFour){
 if (CardThree === CardFour){
 	alert("You found a match")
 }else alert("Sorry try again.")*/
+var cards = ['queen', 'queen', 'king', 'king'];
+var cardsInPlay = [];
 
 var gameBoard = document.getElementById('game-board');
 function makeBoard(){
-	for(var i=1; i<=4; i++){
+	for(var i=0; i<cards.length; i++){
 var makeCard = document.createElement('div'); 
 makeCard.className = 'card';
+makeCard.setAttribute('data-card', cards[i]);
+makeCard.addEventListener('click', isTwoCards);
+gameBoard.appendChild(makeCard);
 gameBoard.appendChild(makeCard);
 }
 }
 
+function isTwoCards(){
+	cardsInPlay.push(this.getAttribute('data-card'));
+	console.log(this.getAttribute('data-card'));
+	if (this.getAttribute('data-card')=='queen'){
+		this.innerHTML = '<img src="playing-card-161484_640.png" alt="Queen of Clubs" />';
+	}
+
+	else {
+		this.innerHTML = '<img src="playing-card-161492_640.png" alt="King of Diamonds" />';
+	}
+
+		if (cardsInPlay.length == 2){
+		isMatch(cardsInPlay);
+	}
+}
+
+
+function isMatch(cards){
+	if (cards[0]==cards[1]){
+	alert("you found a match!")
+}else{ alert("Sorry, no match.")
+}
+	cardsInPlay = [];
+
+}
